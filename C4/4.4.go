1c07384dcab1df82e30878fad6bc0d58d7dccd33
Steven Chung <steven.chung1@gmail.com>
2016-09-25 19:06:40 -0700

    Initial work on balanced tree

--- C4/4.4.go
+++ C4/4.4.go
@@ -17,3 +17,28 @@ type TreeNode struct {
 	Left *TreeNode
 	Right *TreeNode
 }
+
+function returnDepth(node *TreeNode) (int, int) {
+}
+
+func isBalanced(rootNode *TreeNode) bool {
+	arrResults := []int
+	return true
+}
+
+func main() {
+	balancedTree := &TreeNode{Value: 1}
+	balancedTree.Left = &TreeNode{Value: 2}
+	balancedTree.Right = &TreeNode{Value: 3}
+	balancedTree.Left.Left = &TreeNode{Value: 4}
+	balancedTree.Left.Right = &TreeNode{Value: 5}
+
+	unbalancedTree := &TreeNode{Value: 1}
+	unbalancedTree.Left = &TreeNode{Value: 2}
+	unbalancedTree.Right = &TreeNode{Value: 3}
+	unbalancedTree.Right.Right = &TreeNode{Value: 4}
+	unbalancedTree.Right.Right.Right = &TreeNode{Value: 4}
+
+	fmt.Println(isBalanced(balancedTree))
+	fmt.Println(isBalanced(unbalancedTree))
+}
