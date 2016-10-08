7a4bc3daa61dfdf96b4133a6d87d7692838424e8
Steven Chung <steven.chung1@gmail.com>
2016-10-08 10:37:59 -0700

    4.4 py

--- C4/4.4.py
+++ C4/4.4.py
@@ -4,3 +4,29 @@
 # never differ by more than one
 #
 # https://leetcode.com/problems/balanced-binary-tree/
+
+class TreeNode(object):
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+
+def isBalanced(root):
+	def checkNode(node):
+		if node is None:
+			return 0
+
+		left = checkNode(node.left)
+		if left != -1:
+			right = checkNode(node.right)
+			if right != -1:
+				if abs(right - left) > 1:
+					return -1
+				else:
+					return max(left, right) + 1
+			else:
+				return -1
+		else:
+			return -1
+	return checkNode(root) != -1
